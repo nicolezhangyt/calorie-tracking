df37aa4d4a36e91f45a85de5d8a2e71a9f6a1819
yating-zhang <yating.zhang@invoice2go.com>
2021-01-17 00:18:12 +1100

    init project

---
+++ web/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+      </header>
+      <body>
+        body
+      </body>
+    </div>
+  );
+}
+
+export default App;
